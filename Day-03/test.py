c2d6cad1b96aaf0dea5202548f4a16173a39eca3
Rajesh <nrk8008@gmail.com>
2025-06-16 23:37:21 +0530

    add file test.py

---
+++ Day-03/test.py
@@ -0,0 +1,27 @@
+# Global Variables
+
+a = 10
+b = 20
+
+def addition():
+    print(a+b)
+
+def substraction():
+    print(a-b)
+
+addition()
+substraction()
+
+#Local Variables
+def addtion():
+    c = 20
+    d = 30
+    print(c+d)
+
+def substraction():
+    print(c-d)
+
+addition()
+substraction()
+
+
