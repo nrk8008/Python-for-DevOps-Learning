f4d64290ff14cee27d71a21603791df5ad4e73cb
Rajesh <nrk8008@gmail.com>
2025-06-18 19:40:43 +0530

    add files

---
+++ Day-05/calculator_new1.py
@@ -0,0 +1,25 @@
+import sys
+
+def add(num1, num2):
+    return num1 + num2 
+    
+def sub(num1, num2):
+    return num1 - num2
+    
+def mul(num1, num2):  
+    return num1 * num2
+    
+num1 = int(sys.argv[1])
+operation = sys.argv[2]
+num2 = int(sys.argv[3])
+
+if operation == "add":
+    output = add(num1, num2)
+elif operation == "sub":
+    output = sub(num1, num2)
+elif operation == "mul":
+    output = mul(num1, num2)
+else:
+    output = "Invalid operation"
+
+print(output)
