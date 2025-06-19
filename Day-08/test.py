43b2cb3a352d1f6b5c205e55998f2543e9881596
Rajesh <nrk8008@gmail.com>
2025-06-19 15:11:57 +0530

    addfiles

---
+++ Day-08/test.py
@@ -0,0 +1,29 @@
+student1 = "Abhishek"
+student2 = "Ram"
+student3 = "jhon"
+student4 = "tinku"
+
+print(student1)
+print(student2)
+print(student3)
+print(student4)
+
+student_names = ["Abhishek", "Ram","jhon", "tinku" ]
+print(student_names)
+print(type(student_names))
+
+s3_bucket_list = ["nrk_demo", "jenkins_bucket", "tim_demo_bucket"]
+s3_bucket_list.append("new_demo")
+print(s3_bucket_list)
+
+s3_bucket_list.remove("new_demo")
+print(s3_bucket_list)
+
+numbers = [10, 20, 15]
+numbers.sort()
+print(numbers)
+#concatenation
+print(s3_bucket_list[0] + s3_bucket_list[1])
+
+print(len(s3_bucket_list))
+
